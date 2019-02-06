6ee236f23af5f4e9811760dc68047ffd1d01cb7e
DJ Shin <djshin@DJs-MacBook-Pro.local>
2019-02-06 17:03:25 -0500

    something weird just happend that got rid of config.js codes

--- app/screens/feed.js
+++ app/screens/feed.js
@@ -1,14 +1,57 @@
 import React from 'react'
 import {FlatList, StyleSheet, Text, View, Image} from 'react-native';
+import  {f, auth, database, storage} from '../../config/config';
 
 class feed extends React.Component {
 constructor(props){
     super(props);
     this.state = {
-        photo_feed: [0,1,2,3,4],
-        refresh: false
+        photo_feed: [],
+        refresh: false,
+        loading: true 
     }
 }
+
+componentDidMount = () => {
+   this.loadFeed();
+   
+}
+
+loadFeed = () => {
+    this.setState ({
+        refresh:true,
+        photo_feed: []
+    });
+    var that = this;
+
+    database.ref('photos').orderByChild('posted').once('value').then(function(snapshot) {
+        const exists = (snapshot .val() !== null);
+        if(exists) data =  snapshot.val();
+            var photo_feed = that.state.photo_feed; 
+
+            for(var photo in data) {
+                var photoObj = data[photo];
+                database.ref('users').child(photoObj.author).once('value').then(function(snapshot) {
+                    const exists = (snapshot .val() !== null);
+                    if(exists) data =  snapshot.val();
+                        photo_feed.push({
+                            id: photo,
+                            url: photoObj.url,
+                            caption: photoObj.caption,
+                            posted: photoObj.posted,
+                            author: data.username
+                        });
+
+                        that.setState({
+                            refresh: false,
+                            loading: false
+                        });
+
+                }).catch(error => console.log(error));
+            }
+        }).catch(error => console.log(error)); 
+}
+
 
 loadNew = () => {
     this.setState({
