cf39872232aa8db7afd18359b8bff064c7abab77
DJ Shin <djshin@DJs-MacBook-Pro.local>
2019-02-06 15:20:13 -0500

    getting into database

---
+++ app/screens/profile.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import {FlatList, StyleSheet, Text, View, Image} from 'react-native';
+
+class profile extends React.Component {
+constructor(props){
+    super(props);
+}
+render(){
+    return(
+        <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+            <Text>profile!!!!!!!</Text>
+        </View>
+    )
+}
+}
+
+export default profile;
---
+++ app/screens/upload.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import {FlatList, StyleSheet, Text, View, Image} from 'react-native';
+
+class upload extends React.Component {
+constructor(props){
+    super(props);
+}
+render(){
+    return(
+        <View style={{flex:1, justifyContent:'center', alignItems: 'center'}}>
+            <Text>upload!!!!!!!</Text>
+        </View>
+    )
+}
+}
+
+export default upload;
