8bf0e68237778b75b5f489e9999c32c7e643241c
DJ Shin <djatlienriseup@gmail.com>
2019-02-27 22:04:04 -0500

    comment part 1 done

--- app/screens/comments.js
+++ app/screens/comments.js
@@ -1,14 +1,75 @@
 import React from 'react'
-import {FlatList, StyleSheet, Text, View, Image} from 'react-native';
+import { TouchableOpacity, TextInput, KeyboardAvoidingView, FlatList, StyleSheet, Text, View, Image} from 'react-native';
 import  {f, auth, database, storage} from '../../config/config';
 
-class upload extends React.Component {
+class comments extends React.Component {
 constructor(props){
     super(props);
     this.state = {
-        loggedin: false
+        loggedin: false,
+        comments_list: []
     }
 }
+
+checkParams = () => {
+    var params = this.props.navigation.state.params;
+    if(params){
+        if(params.photoId){
+          
+         this.setState({
+             photoId: params.photoId
+         })
+         this.fetchComments(params.photoId);
+        }
+    }
+   }
+
+addCommentToList = (comment_list, data, comment) => {
+    var that = this;
+    var commentObj = data[comment];
+    database.ref('users').child(commentObj.author).child('username').once('value').then(function(snapshot){
+        const exists = (snapshot.val() !== null);
+        if(exists) data = snapshot.val()
+
+        comment_list.push({
+            id: comment,
+            comment: commentObj.comment,
+            posted: that.timeConverter(commentObj.posted),
+            author: data,
+            authorId: commentObj.author
+        });
+
+        that.setState({
+            refresh:false,
+            reloading:false
+        });
+
+    }).catch(error => console.log(error));
+}
+
+fetchComments = (photoId) => {
+    var that = this;
+
+    database.ref('comments').child(photoId).orderByChild('posted').once('value').then(function(snapshot){
+        const exists = (snapshot.val() !== null);
+        if(exists){
+            // add comments to flatlist
+            data = snapshot.val();
+            var comment_list = that.state.comments_list;
+
+            for (var comment in data){
+                that.addCommentToList(comment_list, data, comment);
+            }
+        
+        }else{
+            // are no comments 
+            this.setState({
+                comment_list:[]
+            });
+        }
+    }).catch(error => console.log(error))
+}
+
 
 componentDidMount = () => {
     var that = this;
@@ -25,11 +86,91 @@ componentDidMount = () => {
             });
         }
     });
+    this.checkParams();
+}
+
+s4 = () => {
+    return Math.floor((1 + Math.random()) * 0x10000)
+    .toString(16)
+    .substring(1);
+}
+ 
+uniqueId = () => {
+    return this.s4() + this.s4() + '-' + this.s4() + '-' + this.s4() + '-' 
+    + this.s4() + '-' + this.s4() + '-' + this.s4() + '-' + this.s4();
+}
+
+pluralCheck = (s) => {
+    if(s == 1) {
+        return ' ago';
+    }else{
+        return 's ago'
+    }
+}
+
+timeConverter = (timestamp) => {
+    var a = new Date(timestamp * 1000);
+    var seconds = Math.floor((new Date() - a) / 1000);
+
+    var interval = Math.floor(seconds / 31536000);
+    if (interval > 1){
+        return interval + ' year'+ this.pluralCheck(interval); 
+    }
+    interval = Math.floor(seconds / 2592000);
+    if (interval > 1){
+        return interval + ' month' + this.pluralCheck(interval); 
+    }
+    interval = Math.floor(seconds / 86400);
+    if (interval > 1){
+        return interval + ' day' + this.pluralCheck(interval); 
+    }
+    interval = Math.floor(seconds / 3600);
+    if (interval > 1){
+        return interval + ' hour' + this.pluralCheck(interval); 
+    }
+   
+    interval = Math.floor(seconds / 60);
+    if (interval > 1){
+        return interval + ' minute' + this.pluralCheck(interval);   
+    }
+    return Math.floor(seconds ) + ' seconds' + this.pluralCheck(seconds); 
 }
 
 render(){
     return(
-        <View style={{flex:1, justifyContent:'center', alignItems: 'center'}}>
+        <View style={{flex:1}}>
+                  <View style={{ flexDirection: 'row', height: 70, paddingTop: 30, backgroundColor: 'white', borderColor: 'lightgrey', borderBottomWidth: 0.5, justifyContent: 'center', justifyContent: 'space-between' , alignItems: 'center'}}>
+                <TouchableOpacity style={{width:100}} onPress={()=> this.props.navigation.goBack()} >
+                    <Text style={{fontSize:12, fontWeight:'bold', paddingLeft: 10}}>Go Back</Text>
+                </TouchableOpacity>
+                <Text>Comments</Text>
+                <Text style={{width:100}}></Text>
+                </View>
+
+            { this.state.comments_list.length == 0 ? (
+                // no comments show empty state
+                <Text> No Comments found </Text>
+            ): (
+                // are comments
+            <FlatList
+                refreshing={this.state.refresh}
+                data={this.state.comments_list}
+                keyExtractor={(item, index) => index.toString()}
+                style={{flex:1, backgroundColor: '#eee'}}
+                renderItem={({item, index}) => (
+                <View key={index} style={{width: '100%', overflow:'hidden', marginBottom:5, justifyCotent: 'space-between', borderColor: 'grey'}}>
+                <View>
+                    <Text>{item.posted}</Text>
+                    <TouchableOpacity>
+                        <Text>{item.author}</Text>
+                    </TouchableOpacity>
+                </View>
+                </View>
+                
+                )}
+            />
+            )}
+
              {this.state.loggedin == true ? (
             // logged in
             <Text>Comments</Text>
@@ -46,4 +187,4 @@ render(){
 }
 }
 
-export default upload;
+export default comments;
